b7c3c7b1da37016352898aa8dd7cc4e71a878750
RokasCy <rokas.rukuiza@gmail.com>
2026-02-20 14:41:36 +0200

    jupiter moons + planet rotation

--- scenes/planets/scripts/milkyway_fade.gd
+++ scenes/planets/scripts/milkyway_fade.gd
@@ -11,6 +11,8 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	var fov = (telescope.fov / 50 - 0.05)**2
 	mat.albedo_color.a = clamp(fov, 0, 0.3)
-	print(fov)
 	$MeshInstance3D.set_surface_override_material(0, mat)
-	print(mat.albedo_color.a)
+		
+		
+	
+	
---
+++ scenes/planets/scripts/rotate_planet.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@export var period = 1.0
+@onready var planet = get_parent()
+
+var speed
+func _ready():
+	speed = TAU / period
+
+func _physics_process(delta):
+	planet.rotate_y(speed * delta)
